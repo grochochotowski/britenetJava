115e41fff174f7b1b0925066f287b52c0da3a0a2
grochochotowski <g.m.rochowski@gmail.com>
2024-02-12 16:11:34 +0100

    Create account class

---
+++ task-1/src/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    // Variables
+    private String name;
+    private String surname;
+    private double balance;
+    private String accountNumber;
+
+    // Constructors
+    public Account() { }
+    public Account(String name, String surname, double balance, String accountNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+
+    // Other methods
+}
