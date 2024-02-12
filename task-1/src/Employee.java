90b5ed886798421cc370e4fe61c82ab748a674ee
grochochotowski <g.m.rochowski@gmail.com>
2024-02-12 14:59:08 +0100

    Create base of employee class

---
+++ task-1/src/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+    private String name;
+
+    private String surname;
+    private String PESEL;
+    private String occupation;
+    private double salary;
+    private int experienceStage;
+
+    public Employee(String name, String surname, String PESEL, String occupation, double salary, int experienceStage) {
+        this.name = name;
+        this.surname = surname;
+        this.PESEL = PESEL;
+        this.occupation = occupation;
+        this.salary = salary;
+        this.experienceStage = experienceStage;
+    }
+
+}
