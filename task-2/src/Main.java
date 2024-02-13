21cb7be66d9c41f9140adeb7e6d52c195e84294a
grochochotowski <g.m.rochowski@gmail.com>
2024-02-13 17:40:45 +0100

    Create example cars

--- task-2/src/Main.java
+++ task-2/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Create example cars
+        Car car1 = new Car("Opel", "Corsa", 2011, 150000.50, 9, 180, 40, "ABC 01234");
+        Car car2 = new Car("BMW", "X5", 2023, 76234.50, 10.5, 230, 93, "CBA 09876");
+        Car car3 = new Car("Audi", "A1", 2014, 194000, 8.5, 200, 60, "TST 12A54");
+        Car car4 = new Car("Porshe", "sth", 2024, 0, 15.5, 450, 75, "B0 SPEED");
 
+        
     }
 }
