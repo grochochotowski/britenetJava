608d1d73ca1b67755e092443db9d59a6e24df481
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 22:54:52 +0100

    Remove not needed parts

--- task-3/src/UserAccount.java
+++ task-3/src/UserAccount.java
@@ -10,28 +10,16 @@ public class UserAccount {
     public UserAccount() {
         this.id = -1;
     }
-    public UserAccount(int id, String login, String password, User userData, boolean premium) {
+    public UserAccount(int id, String login, String password, User userData) {
         this.id = id;
         this.login = login;
         this.password = password;
         this.userData = userData;
-        this.premium = premium;
+        this.premium = false;
     }
 
-    // getters
-    public int getId() { return id; }
-    public String getLogin() { return login; }
-    public String getPassword() { return password; }
-    public User getUserData() { return userData; }
-    public boolean isPremium() { return premium; }
-
-
-    // setters
-    public void setLogin(String login) { this.login = login; }
-    public void setPassword(String password) { this.password = password; }
-    public void setUserData(User userData) { this.userData = userData; }
-    public void setPremium(boolean premium) { this.premium = premium; }
-
     // other
-    
+    public void setPassword(String password) { this.password = password; }
+   
+
 }
