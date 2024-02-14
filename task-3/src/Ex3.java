33fbf4a7058cc7a68669bfa8d2b6612d5c337173
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 17:26:07 +0100

    Create example data

--- task-3/src/Ex3.java
+++ task-3/src/Ex3.java
@@ -1,3 +1,11 @@
 public class Ex3 {
+    // Create examples
+    Address add1 = new Address("Bialystok", "Sienkiewicza", "12A", "15-082");
+    Address add2 = new Address("Bialystok", "Poludniowa", "4", "15-341");
 
+    Person person1 = new Person("Michal", "Kazimiruk", 2006, add1);
+    Person person2 = new Person("Karolina", "Mackiewicz", 2015, add2);
+
+    Student student1 = new Student(1, 1, 4.9, person1);
+    Student student2 = new Student(2, 5, 3.4, person2);
 }
