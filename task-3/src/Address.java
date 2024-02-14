0978cd846e9e1bf536e72653177a161b1b88c5e6
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 10:43:19 +0100

    Add getters and setters

--- task-3/src/Address.java
+++ task-3/src/Address.java
@@ -15,6 +15,16 @@ public class Address {
         this.postalCode = postalCode;
     }
 
-    // methods
+    // getters
+    public String getCity() { return city; }
+    public String getStreet() { return street; }
+    public String getNumber() { return number; }
+    public String getPostalCode() { return postalCode; }
 
+    // setters
+    public void setCity(String city) { this.city = city; }
+    public void setStreet(String street) { this.street = street; }
+    public void setNumber(String number) { this.number = number; }
+    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
+    
 }
