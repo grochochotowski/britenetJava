0398c30f3c00dd1b47e782e60a4a62a23b344506
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 17:55:09 +0100

    Create getters and setters for user

--- task-3/src/User.java
+++ task-3/src/User.java
@@ -13,4 +13,19 @@ public class User {
         this.email = email;
         this.phone_number = phone_number;
     }
+
+    // getters
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getEmail() { return email; }
+    public String getPhone_number() { return phone_number; }
+
+    // setters
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPhone_number(String phone_number) { this.phone_number = phone_number; }
+
+    // other
+    
 }
