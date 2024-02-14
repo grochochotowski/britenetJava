c289a4b6cbe5449df6e1d9b27106f6d4e6120aaf
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 17:52:31 +0100

    Create base of user class

--- task-3/src/User.java
+++ task-3/src/User.java
@@ -1,3 +1,16 @@
 public class User {
+    // variables
+    private String name ;
+    private int age;
+    private String email;
+    private String phone_number;
 
+    // constructors
+    public User() { }
+    public User(String name, int age, String email, String phone_number) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
 }
