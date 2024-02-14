0ffbdd2fbffc277a0e610cbb37ad2f90f86d6b39
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 10:51:53 +0100

    Create getters and setters

--- task-3/src/Person.java
+++ task-3/src/Person.java
@@ -13,5 +13,17 @@ public class Person {
         this.address = address;
     }
 
+    // getters
+    public String getName() { return name; }
+    public String getSurname() { return surname; }
+    public static int getYearOfBirth() { return yearOfBirth; }
+    public Address getAddress() { return address; }
+
+    // setters
+    public void setName(String name) { this.name = name; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public static void setYearOfBirth(int yearOfBirth) { Person.yearOfBirth = yearOfBirth; }
+    public void setAddress(Address address) { this.address = address; }
+
     
 }
