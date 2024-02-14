aeeb60e2ddcbf190189a4e0bf907035afebcfaa0
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 12:39:49 +0100

    Create base of ticket class

--- task-3/src/Ticket.java
+++ task-3/src/Ticket.java
@@ -1,3 +1,16 @@
 public class Ticket {
+    // variables
+    private int seat;
+    private Destination destination;
+    private Passenger passenger;
 
+    // constructor
+    public Ticket() { }
+    public Ticket(int seat, Destination destination, Passenger passenger) {
+        this.seat = seat;
+        this.destination = destination;
+        this.passenger = passenger;
+    }
+
+    
 }
