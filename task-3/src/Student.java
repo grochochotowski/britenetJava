ca077003dd064bd93ff1c6fd9acf4fdf4bc9692c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 13:28:22 +0100

    Cereate base of student class

---
+++ task-3/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    // variables
+    private final int id;
+    private int semester;
+    private double average;
+    private final personalData;
+
+    //constructors
+    public Student() {
+    }
+    public Student(int id, int semester, double average) {
+        this.id = id;
+        this.semester = semester;
+        this.average = average;
+    }
+
+    // 
+}
