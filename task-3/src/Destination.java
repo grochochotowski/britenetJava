457451a8288499ba62cc532d71c4280873ab7bc1
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 12:31:32 +0100

    Create base of destination class

--- task-3/src/Destination.java
+++ task-3/src/Destination.java
@@ -1,3 +1,14 @@
 public class Destination {
+    // variables
+    private String from;
+    private String to;
+    private double price;
 
+    // constructors
+    public Destination() { }
+    public Destination(String from, String to, double price) {
+        this.from = from;
+        this.to = to;
+        this.price = price;
+    }
 }
