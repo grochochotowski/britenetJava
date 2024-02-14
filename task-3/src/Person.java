afca944675c9b5fc78727d0201dbdc7bd83cc4ac
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 11:55:10 +0100

    Fix date of birth beeing final instead of static

--- task-3/src/Person.java
+++ task-3/src/Person.java
@@ -5,27 +5,31 @@ public class Person {
     // variables
     private String name;
     private String surname;
-    private static int yearOfBirth;
+    private final int yearOfBirth;
     private Address address;
 
     // constructors
-    public Person() { }
-    public Person(String name, String surname, Address address) {
+    public Person() {
+        this.name = "";
+        this.surname = "";
+        this.yearOfBirth = 0;
+    }
+    public Person(String name, String surname, int yearOfBirth, Address address) {
         this.name = name;
         this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
         this.address = address;
     }
 
     // getters
     public String getName() { return name; }
     public String getSurname() { return surname; }
-    public static int getYearOfBirth() { return yearOfBirth; }
+    public int getYearOfBirth() { return yearOfBirth; }
     public Address getAddress() { return address; }
 
     // setters
     public void setName(String name) { this.name = name; }
     public void setSurname(String surname) { this.surname = surname; }
-    public static void setYearOfBirth(int yearOfBirth) { Person.yearOfBirth = yearOfBirth; }
     public void setAddress(Address address) { this.address = address; }
 
     // other methods
