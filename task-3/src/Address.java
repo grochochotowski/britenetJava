02a2b3b7a6116b9a9a8fffc1024bb4131e417c7a
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 10:36:34 +0100

    Create base of address class

--- task-3/src/Address.java
+++ task-3/src/Address.java
@@ -1,2 +1,20 @@
 public class Address {
+    // variables
+    private String city;
+    private String street;
+    private String number;
+    private String postalCode;
+
+
+    // constructors
+    public Address() { }
+    public Address(String city, String street, String number, String postalCode) {
+        this.city = city;
+        this.street = street;
+        this.number = number;
+        this.postalCode = postalCode;
+    }
+
+    // methods
+
 }
