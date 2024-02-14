96cc9209b524efc09d4fd25626edebf6edd9bbfb
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 18:00:16 +0100

    Create base of user account clas

--- task-3/src/UserAccount.java
+++ task-3/src/UserAccount.java
@@ -1,3 +1,22 @@
 public class UserAccount {
+    // variables
+    private final int id;
+    private String login;
+    private String password;
+    private User userData;
+    private boolean premium;
 
+    // constructors
+    public UserAccount() {
+        this.id = -1;
+    }
+    public UserAccount(int id, String login, String password, User userData, boolean premium) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.userData = userData;
+        this.premium = premium;
+    }
+
+    
 }
