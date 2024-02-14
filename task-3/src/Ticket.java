bef259fe6f590781a7975d334d220b29b000d4c6
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 12:41:42 +0100

    Create getters and setters for ticket class

--- task-3/src/Ticket.java
+++ task-3/src/Ticket.java
@@ -12,5 +12,13 @@ public class Ticket {
         this.passenger = passenger;
     }
 
-    
+    // getters
+    public int getSeat() { return seat; }
+    public Destination getDestination() { return destination; }
+    public Passenger getPassenger() { return passenger; }
+
+    // setters
+    public void setSeat(int seat) { this.seat = seat; }
+    public void setDestination(Destination destination) { this.destination = destination; }
+    public void setPassenger(Passenger passenger) { this.passenger = passenger; }
 }
