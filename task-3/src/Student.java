6bd5ed82386070b340c94be497a19ea5f752b785
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 13:29:42 +0100

    Fix base student class

--- task-3/src/Student.java
+++ task-3/src/Student.java
@@ -3,16 +3,19 @@ public class Student {
     private final int id;
     private int semester;
     private double average;
-    private final personalData;
+    private final Person personalData;
 
     //constructors
     public Student() {
+        this.id = -1;
+        this.personalData = new Person();
     }
-    public Student(int id, int semester, double average) {
+    public Student(int id, int semester, double average, Person personalData) {
         this.id = id;
         this.semester = semester;
         this.average = average;
+        this.personalData = personalData;
     }
 
-    // 
+    //
 }
