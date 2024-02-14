25786d1e8b9d34fbdf096136645f2185111c768c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 12:16:37 +0100

    create base of passenger class

--- task-3/src/Destination.java
+++ task-3/src/Destination.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Destination {
+public class Destination {
+
 }
--- task-3/src/Passenger.java
+++ task-3/src/Passenger.java
@@ -1,2 +1,27 @@
 public class Passenger {
+    // variables
+    private String name;
+    private String surname;
+    private boolean discount;
+
+    // constructors
+    public Passenger() { }
+    public Passenger(String name, String surname, boolean discount) {
+        this.name = name;
+        this.surname = surname;
+        this.discount = discount;
+    }
+
+    // getters
+    public String getName() { return name; }
+    public String getSurname() { return surname; }
+    public boolean isDiscount() { return discount; }
+
+    // setters
+    public void setName(String name) { this.name = name; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public void setDiscount(boolean discount) { this.discount = discount; }
+
+    // other
+
 }
