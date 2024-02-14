4ddef46bed5de6b6c9919615e7d238e9d3d2c15f
grochochotowski <g.m.rochowski@gmail.com>
2024-02-14 10:49:23 +0100

    Create base of person class

--- task-3/src/Person.java
+++ task-3/src/Person.java
@@ -1,2 +1,17 @@
 public class Person {
+    // variables
+    private String name;
+    private String surname;
+    private static int yearOfBirth;
+    private Address address;
+
+    // constructors
+    public Person() { }
+    public Person(String name, String surname, Address address) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+    }
+
+    
 }
