28fdf988d786d83e987a84b05e844a6d54d8e9c7
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 19:15:00 +0100

    Create constructor for train

--- task-4/src/Train.java
+++ task-4/src/Train.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Train {
@@ -6,6 +7,14 @@ public class Train {
     private String to;
     private double price;
     private List<TrainCarriage> carriageList;
+
+    // constructor
+    public Train(String from, String to, double price) {
+        this.from = from;
+        this.to = to;
+        this.price = price;
+        this.carriageList = new ArrayList<>();
+    }
 
     
 }
