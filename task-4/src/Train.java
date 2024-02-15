32d31d85c8d47be5ffc288bfc9f8db5300325c4c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-15 19:14:11 +0100

    Add variables for train class

--- task-4/src/Train.java
+++ task-4/src/Train.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class Train {
+    // variables
+    private String from;
+    private String to;
+    private double price;
+    private List<TrainCarriage> carriageList;
+
+    
 }
