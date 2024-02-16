de366b76fd3fc4713200bd67e057d4297238f22e
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 13:19:13 +0100

    Create employee variables

--- task-5/src/Employee.java
+++ task-5/src/Employee.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    // variables
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+    private String country;
+    private String city;
+    private String street;
+    private double monthlySalaryGross;
+
+    
 }
