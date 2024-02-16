2d71ee86c2f07c77dd5d843671da9c3efa761aba
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 13:50:08 +0100

    Create example employees

--- task-5/src/Ex1.java
+++ task-5/src/Ex1.java
@@ -1,5 +1,8 @@
 public class Ex1 {
     public static void main(String[] args) {
+        // example data
+        Employee employee1 = new Employee("Maciej", "Makarewicz", 2000, "web dev", "Poland", "Bialystok", "Sienkiewicza", 10500.50);
+        Employee employee2 = new Employee("Karol", "Szycko", 1999, "shop assistant", "Poland", "Bialystok", "Mickiewicza", 5000.20);
 
     }
 }
