156cb799d2962947c03aa5ac52cfdfb91b32fa07
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 13:51:11 +0100

    Test displaying methds

--- task-5/src/Ex1.java
+++ task-5/src/Ex1.java
@@ -4,5 +4,11 @@ public class Ex1 {
         Employee employee1 = new Employee("Maciej", "Makarewicz", 2000, "web dev", "Poland", "Bialystok", "Sienkiewicza", 10500.50);
         Employee employee2 = new Employee("Karol", "Szycko", 1999, "shop assistant", "Poland", "Bialystok", "Mickiewicza", 5000.20);
 
+        // display methods
+        System.out.println(employee1.toString());
+        employee2.displayAllData();
+        employee1.displayAddress();
+
+        
     }
 }
