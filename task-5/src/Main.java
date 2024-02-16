0ea1dc0c148b51f15ec7672e47f2e8616c491e38
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 14:01:29 +0100

    Rename files

--- task-5/src/Main.java
+++ task-5/src/Main.java
@@ -1,4 +1,4 @@
-public class Ex1 {
+public class Main {
     public static void main(String[] args) {
         // example data
         Employee employee1 = new Employee("Maciej", "Makarewicz", 2000, "web dev", "Poland", "Bialystok", "Sienkiewicza", 10500.50);
