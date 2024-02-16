2e006eb60a663ab09f596e19e216e1cbdfa0834c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 13:19:47 +0100

    Add constructor

--- task-5/src/Employee.java
+++ task-5/src/Employee.java
@@ -8,5 +8,14 @@ public class Employee {
     private String street;
     private double monthlySalaryGross;
 
-    
+    // constructor
+    public Employee(String name, String surname, int yearOfBirth, String country, String city, String street, double monthlySalaryGross) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.monthlySalaryGross = monthlySalaryGross;
+    }
 }
