7978d58ca77c95cdbce7fd983a8df579a88a5f02
grochochotowski <g.m.rochowski@gmail.com>
2024-02-16 14:36:56 +0100

    Create base of classes

--- task-5/src/EmployeeHelper.java
+++ task-5/src/EmployeeHelper.java
@@ -1,3 +1,13 @@
 public class EmployeeHelper {
-    
+    public static double calculateYearlySalary(double monthlySalary) {
+
+    }
+
+    public static boolean isEligibleForPromotion(int yearsWorked) {
+
+    }
+
+    public static void generateEmployeeID(String firstName, String lastName) {
+        
+    }
 }
