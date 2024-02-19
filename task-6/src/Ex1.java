7a55c964f98c1b7bb62e426843ad634db4866d78
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:34:14 +0100

    Create example workers

--- task-6/src/Ex1.java
+++ task-6/src/Ex1.java
@@ -1,5 +1,10 @@
 public class Ex1 {
     public static void main(String[] args) {
-
+        // create example data
+        BlueCollarWorker blueWorker = new BlueCollarWorker("Blue", "Worker", "00000000000",
+                1999, 2010,'m', "H&M", "shop assistant",
+                27.70, 25.5);
+        WhiteCollarWorker whiteWorker = new WhiteCollarWorker("White", "Worker", "11111111111",
+                2995, 2014,'f', "Google", "dev", 9999.99);
     }
 }
