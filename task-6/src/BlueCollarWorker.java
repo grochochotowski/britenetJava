23315845a043bea8bb10e5a79467bac75597bfd3
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:10:56 +0100

    create super constructor

--- task-6/src/BlueCollarWorker.java
+++ task-6/src/BlueCollarWorker.java
@@ -4,6 +4,13 @@ public class BlueCollarWorker extends Worker{
     private double hoursWorked;
 
     // constructor
+    public BlueCollarWorker(String name, String surname, String PESEL, int dateOfBirth,
+                            int dateOfEmployment, char sex, String company, String occupation,
+                            double paymentPerHour, double hoursWorked) {
+        super(name, surname, PESEL, dateOfBirth, dateOfEmployment, sex, company, occupation);
+        this.paymentPerHour = paymentPerHour;
+        this.hoursWorked = hoursWorked;
+    }
 
 
     // getters
