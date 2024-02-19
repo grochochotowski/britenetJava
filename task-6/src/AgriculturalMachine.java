c23a779bf55d8f975b446358e59916c2066c3dcc
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 16:20:46 +0100

    copy and modify AgriculturalMachine

--- task-6/src/AgriculturalMachine.java
+++ task-6/src/AgriculturalMachine.java
@@ -1,2 +1,26 @@
-public class AgriculturalMachine {
+public class AgriculturalMachine extends Vehicle{
+    // variable
+    private String subCategory;
+
+    // constructor
+    public AgriculturalMachine(String subCategory) {
+        this.subCategory = subCategory;
+    }
+    public AgriculturalMachine(String model, String mark, String color, String state, double price, String subCategory) {
+        super(model, mark, color, state, price);
+        this.subCategory = subCategory;
+    }
+
+    // getter & setter
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Car{" +
+                super.toString() +
+                "type='" + type + '\'' +
+                '}';
+    }
 }
