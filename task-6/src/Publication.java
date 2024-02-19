a55b1e11332fa14c8b5adee0a767921a60ed04c1
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:41:01 +0100

    Create pubblication class

---
+++ task-6/src/Publication.java
@@ -0,0 +1,9 @@
+public class Publication {
+    // variables
+    private String title;
+    private String publisher;
+    private int year;
+    private String ident;
+    private double price;
+    private int quantity;
+}
