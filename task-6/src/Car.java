7b7ff2fe42ba994d998d6998d81d9ff313980f7d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 16:17:08 +0100

    add constrcutro

--- task-6/src/Car.java
+++ task-6/src/Car.java
@@ -1,2 +1,23 @@
-public class Car {
+public class Car extends Vehicle{
+    // variable
+    private String type;
+
+    // constructor
+    public Car(String type) {
+        this.type = type;
+    }
+    public Car(String model, String mark, String color, String state, double price, String type) {
+        super(model, mark, color, state, price);
+        this.type = type;
+    }
+
+    // getter & setter
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
