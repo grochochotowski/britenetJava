d3a85f0692d457df181558532dff235fb2327ec5
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:48:39 +0100

    Create subclass

---
+++ task-6/src/Book.java
@@ -0,0 +1,5 @@
+public class Book extends Publication {
+    public Book(String title, String publisher, int year, String ident, double price, int quantity) {
+        super(title, publisher, year, ident, price, quantity);
+    }
+}
