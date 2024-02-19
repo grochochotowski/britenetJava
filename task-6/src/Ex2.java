e007f836ba023fb08e105cd169e6980efff9cc6c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:54:22 +0100

    Test methods

--- task-6/src/Ex2.java
+++ task-6/src/Ex2.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class Ex2 {
     public static void main(String[] args) {
         // create example data
         Book book = new Book("BookTitle", "Sth", 2024, "ISBN", 99.99, 0, "Smn");
-        
-        // test toString
 
         // test display
+        book.display();
+        System.out.print("\n");
 
-        // test
+        // test buying and selling
+        book.buy(10);
+        book.sell(4);
 
-        // test children methods
+        // display
+        book.display();
     }
 }
