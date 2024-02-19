a31cdcc3f9a5df3b4c33c79fdafc01b67601dcff
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 16:13:29 +0100

    add getters and setters

--- task-6/src/Vehicle.java
+++ task-6/src/Vehicle.java
@@ -15,5 +15,17 @@ public class Vehicle {
         this.price = price;
     }
 
-    
+    // getters
+    public String getModel() { return model; }
+    public String getMark() { return mark; }
+    public String getColor() { return color; }
+    public String getState() { return state; }
+    public double getPrice() { return price; }
+
+    // setters
+    public void setModel(String model) { this.model = model; }
+    public void setMark(String mark) { this.mark = mark; }
+    public void setColor(String color) { this.color = color; }
+    public void setState(String state) { this.state = state; }
+    public void setPrice(double price) { this.price = price; }
 }
