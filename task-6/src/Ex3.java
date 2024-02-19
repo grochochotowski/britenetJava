5069bbbea8411e7c3ea284ea6f88549c0cf78f34
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 16:25:33 +0100

    Create ex data

--- task-6/src/Ex3.java
+++ task-6/src/Ex3.java
@@ -1,7 +1,10 @@
 public class Ex3 {
     public static void main(String[] args) {
         // create example data
-
+        Vehicle vehicle = new Vehicle("Plane", "sth", "white", "sold", 30000000);
+        Car car = new Car("Opel", "Corsa", "red", "not-sold", 15000, "supermini");
+        AgriculturalMachine agriculturalMachine = new AgriculturalMachine("Lamborghini", "spire f", "yellow", "not-sold", 300000);
+        
         // test display
 
     }
