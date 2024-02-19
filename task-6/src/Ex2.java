c96cb266eeb28127c12d19b402c16af80c985b15
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:52:39 +0100

    Create example book

--- task-6/src/Ex2.java
+++ task-6/src/Ex2.java
@@ -1,7 +1,8 @@
 public class Ex2 {
     public static void main(String[] args) {
         // create example data
-
+        Book book = new Book("BookTitle", "Sth", 2024, "ISBN", 99.99, 0, "Smn");
+        
         // test toString
 
         // test display
