9a86d49cf87928a7c4970de70fff28948ea5eb9d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:09:29 +0100

    Create constructor

--- task-6/src/CommissionWorker.java
+++ task-6/src/CommissionWorker.java
@@ -2,4 +2,9 @@ public class CommissionWorker extends Worker{
 
     // variables
     private double[] monthlySalaries;
+
+    public CommissionWorker(String name, String surname, String PESEL, int dateOfBirth, int dateOfEmployment, char sex, String company, String occupation, double[] monthlySalaries) {
+        super(name, surname, PESEL, dateOfBirth, dateOfEmployment, sex, company, occupation);
+        this.monthlySalaries = monthlySalaries;
+    }
 }
