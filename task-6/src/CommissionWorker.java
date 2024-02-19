a8251643a140af576c978e2d05de6feca825e0ba
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:10:15 +0100

    Create getters and setters

--- task-6/src/CommissionWorker.java
+++ task-6/src/CommissionWorker.java
@@ -3,8 +3,13 @@ public class CommissionWorker extends Worker{
     // variables
     private double[] monthlySalaries;
 
+    // constructor
     public CommissionWorker(String name, String surname, String PESEL, int dateOfBirth, int dateOfEmployment, char sex, String company, String occupation, double[] monthlySalaries) {
         super(name, surname, PESEL, dateOfBirth, dateOfEmployment, sex, company, occupation);
         this.monthlySalaries = monthlySalaries;
     }
+
+    // getters & setters
+    public double[] getMonthlySalaries() { return monthlySalaries; }
+    public void setMonthlySalaries(double[] monthlySalaries) { this.monthlySalaries = monthlySalaries; }
 }
