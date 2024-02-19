7917ea73ab37a2bbb8b7cc0c4ef1ddf4d4b89dcf
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 15:30:10 +0100

    Create main ex file

---
+++ task-6/src/Ex2.java
@@ -0,0 +1,13 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        // create example data
+
+        // test toString
+
+        // test display
+
+        // test 
+
+        // test children methods
+    }
+}
