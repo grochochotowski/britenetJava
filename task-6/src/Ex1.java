5ba51c522a777b3b74668cebc82bf86817479e53
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:55:24 +0100

    test children methods

--- task-6/src/Ex1.java
+++ task-6/src/Ex1.java
@@ -1,11 +1,11 @@
 public class Ex1 {
     public static void main(String[] args) {
         // create example data
+        WhiteCollarWorker whiteWorker = new WhiteCollarWorker("White", "Worker", "11111111111",
+                1995, 2014,'f', "Google", "dev", 9999.99);
         BlueCollarWorker blueWorker = new BlueCollarWorker("Blue", "Worker", "00000000000",
                 1999, 2010,'m', "H&M", "shop assistant",
                 27.70, 25.5);
-        WhiteCollarWorker whiteWorker = new WhiteCollarWorker("White", "Worker", "11111111111",
-                1995, 2014,'f', "Google", "dev", 9999.99);
 
         // test toString
         System.out.println("===============TO STRING===============");
@@ -31,5 +31,9 @@ public class Ex1 {
         System.out.println("white: " + whiteWorker.calculateTimeToRetirement());
         System.out.println("blue: " + blueWorker.calculateTimeToRetirement());
 
+        // test children methods
+        System.out.println("\n===============CHILDREN METHODS===============");
+        System.out.println("white (calculateAnnualSalary): " + whiteWorker.calculateAnnualSalary());
+        System.out.println("blue: (calculatePayment)" + blueWorker.calculatePayment());
     }
 }
