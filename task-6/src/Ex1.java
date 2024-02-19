bb839df104dac84b8d190cef0e0e5cbe0a583c93
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:36:00 +0100

    Test to string

--- task-6/src/Ex1.java
+++ task-6/src/Ex1.java
@@ -6,5 +6,10 @@ public class Ex1 {
                 27.70, 25.5);
         WhiteCollarWorker whiteWorker = new WhiteCollarWorker("White", "Worker", "11111111111",
                 2995, 2014,'f', "Google", "dev", 9999.99);
+
+        // test toString
+        System.out.println("===============TO STRING===============");
+        System.out.printf("white:\n%s", whiteWorker.toString());
+        System.out.printf("\nblue:\n%s", blueWorker.toString());
     }
 }
