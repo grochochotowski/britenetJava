4d4cbac91cf0579b0a781c5246fefe746eb36d31
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:24:19 +0100

    Copy class of blueCollarWorker

--- task-6/src/WhiteCollarWorker.java
+++ task-6/src/WhiteCollarWorker.java
@@ -1,2 +1,48 @@
-public class WhiteCollarWorker {
+public class BlueCollarWorker extends Worker{
+    // variables
+    private double paymentPerHour;
+    private double hoursWorked;
+
+    // constructor
+    public BlueCollarWorker(String name, String surname, String PESEL, int dateOfBirth,
+                            int dateOfEmployment, char sex, String company, String occupation,
+                            double paymentPerHour, double hoursWorked) {
+        super(name, surname, PESEL, dateOfBirth, dateOfEmployment, sex, company, occupation);
+        this.paymentPerHour = paymentPerHour;
+        this.hoursWorked = hoursWorked;
+    }
+
+    // getters
+    public double getPaymentPerHour() { return paymentPerHour; }
+    public double getHoursWorked() { return hoursWorked; }
+
+    // setters
+    public void setPaymentPerHour(double paymentPerHour) { this.paymentPerHour = paymentPerHour; }
+    public void setHoursWorked(double hoursWorked) { this.hoursWorked = hoursWorked; }
+
+    // toString & display
+    @Override
+    public String toString() {
+        String text = "BlueCollarWorker" +
+                super.toString() +
+                "paymentPerHour=" + paymentPerHour +
+                ", hoursWorked=" + hoursWorked +
+                '}';
+        return text;
+    }
+
+    public void display() {
+        super.display();
+        String text = """
+               PAYMENT: %.2f/h
+               WORKED: %.2f
+               """;
+        System.out.printf(text, paymentPerHour, hoursWorked);
+    }
+
+    // calculate
+    public double calculatePayment() {
+        return hoursWorked * paymentPerHour;
+    }
+
 }
