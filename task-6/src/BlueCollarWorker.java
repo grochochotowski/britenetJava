695867020e7a837877a8b127e6ee6c74214b7343
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 14:05:41 +0100

    Divide class by sections

--- task-6/src/BlueCollarWorker.java
+++ task-6/src/BlueCollarWorker.java
@@ -1,2 +1,20 @@
 public class BlueCollarWorker extends Worker{
+    // variables
+
+
+    // constructor
+
+
+    // getters
+
+
+    // setters
+
+
+    // toString & display
+
+
+    // calculate
+
+    
 }
