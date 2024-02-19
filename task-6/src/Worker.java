74b0c3fcfb518f1973a67b778af048e0da1b508f
grochochotowski <g.m.rochowski@gmail.com>
2024-02-19 13:38:57 +0100

    Create worker variables

--- task-6/src/Worker.java
+++ task-6/src/Worker.java
@@ -1,2 +1,12 @@
 public class Worker {
+    // variables
+    private String name;
+    private String surname;
+    private String PESEL;
+    private int dateOfBirth;
+    private int dateOfEmployment;
+    private char sex;
+    private String company;
+    private String occuption;
+
 }
