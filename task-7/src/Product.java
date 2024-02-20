e8b03fef0706d6bca1c84888c00945283b42bddd
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:23:24 +0100

    Create product class

--- task-7/src/Product.java
+++ task-7/src/Product.java
@@ -1,2 +1,12 @@
-public class Product {
+public abstract class Product {
+    protected String name;
+    protected double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public abstract double CalculatePrice();
+    public abstract void display();
 }
