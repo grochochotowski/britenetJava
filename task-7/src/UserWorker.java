063e845b9cb5421a00a9f76d0434e331aac3ca48
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:47:46 +0100

    Create worker class

--- task-7/src/UserWorker.java
+++ task-7/src/UserWorker.java
@@ -1,2 +1,18 @@
 public class UserWorker extends User{
+    private String position;
+
+    public UserWorker(String name, String surname, double fee, String position) {
+        super(name, surname, 10);
+        this.position = position;
+    }
+
+    public String returnDescription() {
+        String text =
+                """
+                NAME: %s %s
+                FEE: %.2fPLN
+                POSITION: %s
+                """;
+        return String.format(text,name, surname, fee, position);
+    };
 }
