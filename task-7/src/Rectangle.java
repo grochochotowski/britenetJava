59a66ffae99c8e1d5a77bb69f1a6e3135e96de42
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:04:54 +0100

    Copy from square and create rectangle class

--- task-7/src/Rectangle.java
+++ task-7/src/Rectangle.java
@@ -1,2 +1,17 @@
 public class Rectangle {
+    private double side1Length;
+    private double side2Length;
+
+    public Rectangle(double side1Length, double side2Length) {
+        this.side1Length = side1Length;
+        this.side2Length = side2Length;
+    }
+
+    // methods
+    public double Area() {
+        return side1Length*side2Length;
+    };
+    public double Circumference () {
+        return (2*side1Length) + (2*side2Length);
+    };
 }
