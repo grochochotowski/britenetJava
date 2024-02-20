e18853cd32dd276963bd318c1c6d1e84f96c1f21
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:26:23 +0100

    Create food product class

--- task-7/src/FoodProduct.java
+++ task-7/src/FoodProduct.java
@@ -1,2 +1,24 @@
 public class FoodProduct extends Product {
+    public String expires;
+    public String type;
+
+    public FoodProduct(String name, double price, String expires, String type) {
+        super(name, price);
+        this.expires = expires;
+        this.type = type;
+    }
+
+    public double CalculatePrice() {
+        return price;
+    }
+    public void display() {
+        String text =
+                """
+                NAME: %s
+                TYPE: %s
+                PRICE: %.2f
+                EXPIRES: %s
+                """;
+        System.out.printf(text, name, type, price, expires);
+    }
 }
