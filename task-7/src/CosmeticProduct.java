d19babc1d64447dd0bcb191dee9daa148cd986e5
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:30:16 +0100

    Create cosmetic product class

--- task-7/src/CosmeticProduct.java
+++ task-7/src/CosmeticProduct.java
@@ -1,2 +1,24 @@
 public class CosmeticProduct extends Product{
+    public String ingredients;
+    public String purpose;
+
+    public CosmeticProduct(String name, double price, String ingredients, String purpose) {
+        super(name, price);
+        this.ingredients = ingredients;
+        this.purpose = purpose;
+    }
+
+    public double CalculatePrice() {
+        return price;
+    }
+    public void display() {
+        String text =
+                """
+                NAME: %s
+                PRICE: %.2f
+                INGREDIENTS: %s
+                PURPOSE: %s
+                """;
+        System.out.printf(text, name, price, ingredients, purpose);
+    }
 }
