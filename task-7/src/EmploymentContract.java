e4f8d5da42e3bd581ee2992433defb65562a4b4c
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:43:59 +0100

    copy classes from invoice and modify them

--- task-7/src/EmploymentContract.java
+++ task-7/src/EmploymentContract.java
@@ -5,7 +5,6 @@ public class EmploymentContract extends Document{
     private String endDate;
 
     // constructor
-
     public EmploymentContract(String date, String city, String contractor1, String contractor2, boolean isSigned,
                               double salary, String startDate, String endDate) {
         super(date, city, contractor1, contractor2, isSigned);
@@ -14,5 +13,30 @@ public class EmploymentContract extends Document{
         this.endDate = endDate;
     }
 
-    
+    // display
+    public void displayDetailedData() {
+        String text =
+                """
+                Type: Contract of employment
+                Time: %s - %s
+                Where: %s
+                When: %s
+                Between: %s and %s
+                Salary: %.2f
+                isSigned: %s
+                """;
+        System.out.printf(text, startDate, endDate, city, date, contractor1, contractor2, salary, isSigned ? "YES" : "NO");
+    };
+
+    // return
+    public String returnDocumentType() {
+        return "Document type: Contract of employment";
+    };
+
+    // sign
+    public void Sign() {
+        this.isSigned = true;
+        System.out.println("Contract of employment has been signed");
+    };
+
 }
