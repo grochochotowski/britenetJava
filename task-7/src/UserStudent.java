6b338a02dc9ee05b8d162bc99cd4ae91c3503a30
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:45:40 +0100

    Create return desctiptin method

--- task-7/src/UserStudent.java
+++ task-7/src/UserStudent.java
@@ -1,2 +1,18 @@
 public class UserStudent extends User{
+    private int year;
+
+    public UserStudent(String name, String surname, double fee, int year) {
+        super(name, surname,0);
+        this.year = year;
+    }
+
+    public String returnDescription() {
+        String text =
+                """
+                NAME: %s %s
+                FEE: %.2fPLN (student plan)
+                SCHOOL YEAR: %d
+                """;
+        return String.format(text,name, surname, fee, year);
+    };
 }
