2361af1e1c9f4e85bfeb5d7b0aafdca83a702bfb
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:15:47 +0100

    test methods

--- task-7/src/Ex2.java
+++ task-7/src/Ex2.java
@@ -5,8 +5,15 @@ public class Ex2 {
         Rectangle rectangle = new Rectangle(3, 9.5);
         Circle circle = new Circle(5);
         Triangle triangle = new Triangle(2, 3);
-        
-        // test
 
+        // test
+        System.out.println("Area: " + square.Area());
+        System.out.println("Circumference: " + square.Circumference() + "\n");
+        System.out.println("Area: " + rectangle.Area());
+        System.out.println("Circumference: " + rectangle.Circumference() + "\n");
+        System.out.println("Area: " + circle.Area());
+        System.out.println("Circumference: " + circle.Circumference() + "\n");
+        System.out.println("Area: " + triangle.Area());
+        System.out.println("Circumference: " + triangle.Circumference() + "\n");
     }
 }
