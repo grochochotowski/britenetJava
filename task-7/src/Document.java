573ac1d61c87bbee0a345c3c51dea210de42df85
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:22:17 +0100

    Add constructor

--- task-7/src/Document.java
+++ task-7/src/Document.java
@@ -5,4 +5,13 @@ public class Document {
     protected String contractor1;
     protected String contractor2;
     protected boolean isSigned ;
+
+    // constructors
+    public Document(String date, String city, String contractor1, String contractor2, boolean isSigned) {
+        this.date = date;
+        this.city = city;
+        this.contractor1 = contractor1;
+        this.contractor2 = contractor2;
+        this.isSigned = false;
+    }
 }
