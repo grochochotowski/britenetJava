0158bf93bd7114970ebc3d2d52e6520c67248a9d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:40:19 +0100

    Create base of user class

--- task-7/src/User.java
+++ task-7/src/User.java
@@ -1,2 +1,13 @@
-public class User {
+public abstract class User {
+    protected String name;
+    protected String surname;
+    protected double fee;
+
+    public User(String name, String surname, double fee) {
+        this.name = name;
+        this.surname = surname;
+        this.fee = fee;
+    }
+
+    
 }
