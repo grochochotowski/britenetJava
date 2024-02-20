6f80625c04154844b2d311fc882bc9792d1a8a32
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:38:53 +0100

    add detailed data

--- task-7/src/Invoice.java
+++ task-7/src/Invoice.java
@@ -12,7 +12,17 @@ public class Invoice extends Document{
 
     // display
     public void displayDetailedData() {
-
+        String text =
+                """
+                Type: Invoice
+                Number: %s
+                Where: %s
+                When: %s
+                Between: %s and %s
+                Total value: %s
+                isSigned: %s
+                """;
+        System.out.printf(text, number, city, date, contractor1, contractor2, value, isSigned ? "YES" : "NO");
     };
 
     // return
