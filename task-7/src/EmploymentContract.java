5830950c0e44d47aad0a1646b4408c62add74992
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:42:19 +0100

    add constructor

--- task-7/src/EmploymentContract.java
+++ task-7/src/EmploymentContract.java
@@ -1,5 +1,18 @@
-public class EmploymentContract {
+public class EmploymentContract extends Document{
+    // variables
     private double salary;
     private String startDate;
     private String endDate;
+
+    // constructor
+
+    public EmploymentContract(String date, String city, String contractor1, String contractor2, boolean isSigned,
+                              double salary, String startDate, String endDate) {
+        super(date, city, contractor1, contractor2, isSigned);
+        this.salary = salary;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    
 }
