7c0682bd0a94c771d061002da3be42d818538a00
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:14:12 +0100

    Create variables

--- task-7/src/Ex2.java
+++ task-7/src/Ex2.java
@@ -1,8 +1,11 @@
 public class Ex2 {
     public static void main(String[] args) {
         // example shapes
-
-
+        Square square = new Square(5.5);
+        Rectangle rectangle = new Rectangle(3, 9.5);
+        Circle circle = new Circle(5);
+        Triangle triangle = new Triangle(2, 3);
+        
         // test
 
     }
