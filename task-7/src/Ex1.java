fc0dcd3ab23d0d3f992eb54377843b9c4553c511
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:50:47 +0100

    Create example data

--- task-7/src/Ex1.java
+++ task-7/src/Ex1.java
@@ -1,7 +1,10 @@
 public class Ex1 {
     public static void main(String[] args) {
         // example documents
-        Invoice invoice = new Invoice("31-02-2024", "Bialystok", "BestCompany", "Adam", 1, 99.99 );
+        Invoice invoice = new Invoice("31-02-2024", "Bialystok", "BestCompany", "Adam",
+                1, 99.99 );
+        EmploymentContract contract = new EmploymentContract ("03-02-2024", "Warszawa", "iDontKnow",
+                "Adam", 40000, "01-01-2024", "02-03-2024" );
 
     }
 }
