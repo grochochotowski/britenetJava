b4f3aad5c10561a953daab24af7cf55ea3adb2a7
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:49:38 +0100

    Create example users

--- task-7/src/Ex4.java
+++ task-7/src/Ex4.java
@@ -1,7 +1,8 @@
 public class Ex4 {
     public static void main(String[] args) {
         // example shapes
-
+        UserStudent student = new UserStudent("Jack", "Smith", 3);
+        UserWorker worker = new UserWorker("John", "Not", "CEO");
 
         // test
 
