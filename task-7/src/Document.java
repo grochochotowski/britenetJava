728c997721b89dd2b969045c4fe792e3cb5fc53a
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 14:20:19 +0100

    Create document class and variables

---
+++ task-7/src/Document.java
@@ -0,0 +1,8 @@
+public class Document {
+    // variables
+    protected String date;
+    protected String city;
+    protected String contractor1;
+    protected String contractor2;
+    protected boolean isSigned ;
+}
