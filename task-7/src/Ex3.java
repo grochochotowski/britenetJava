8ad98af0be92852bcfd15e4c6b7c3f27804a25ea
grochochotowski <g.m.rochowski@gmail.com>
2024-02-20 15:32:48 +0100

    Create example products

--- task-7/src/Ex3.java
+++ task-7/src/Ex3.java
@@ -1,10 +1,13 @@
 public class Ex3 {
     public static void main(String[] args) {
         // example shapes
-
+        FoodProduct food = new FoodProduct("Banan", 1.99, "2025", "Owoc");
+        ElectronicProduct elec = new ElectronicProduct("PC", 1999.99, 24, "HP");
+        CosmeticProduct cosm = new CosmeticProduct("Krem", 1.99, "Mleko i cos - nie wiem, nie znam sie",
+                "Twarz");
 
         // test
-
+        
 
     }
 }
