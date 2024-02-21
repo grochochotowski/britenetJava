ca9c8127810aae62fef389eaf855a91c0d3afbac
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 19:10:24 +0100

    Creatre square classes

--- task-8/src/Square.java
+++ task-8/src/Square.java
@@ -1,16 +1,18 @@
 public class Square implements GeometricShape {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
     @Override
     public void calculateArea() {
-
+        System.out.println("Area: " + (side*side));
     }
 
     @Override
     public void calculateCircumference() {
-
-    }
-
-    @Override
-    public void draw() {
-
+        System.out.println("Circumference: " + (4*side));
     }
 }
