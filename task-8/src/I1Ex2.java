7b25e80f5525a3d2681d794c1476752c9b426467
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 14:38:45 +0100

    make greet methd

--- task-8/src/I1Ex2.java
+++ task-8/src/I1Ex2.java
@@ -1,8 +1,10 @@
 public class I1Ex2 {
-    public static void greet() {
-
+    public static void greet(Language... languages) {
+        for (Language l : languages) {
+            System.out.println(l.sayGreeting());
+        }
     }
     public static void main(String[] args) {
-
+        greet(new English(), new Spanish(), new Japanese(), new Polish());
     }
 }
