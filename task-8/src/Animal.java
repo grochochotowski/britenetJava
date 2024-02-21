5386c64f1bfcdc0533ba165b3bb58e0956379de8
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 14:06:24 +0100

    Create animal class

---
+++ task-8/src/Animal.java
@@ -0,0 +1,9 @@
+public abstract class Animal {
+    private String name = "bez imienia";
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract String getTyp();
+}
