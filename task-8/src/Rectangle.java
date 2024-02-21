e4146d379e332336b792fbd58e20079c192f0bd2
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 19:03:34 +0100

    add rectangle sides and constructor

--- task-8/src/Rectangle.java
+++ task-8/src/Rectangle.java
@@ -1,4 +1,13 @@
 public class Rectangle implements GeometricShape {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
     public void calculateArea() {
 
