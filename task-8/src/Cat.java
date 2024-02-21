839d749fbc5bc057c8fb00af2a546d53b9f15dc1
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 18:53:46 +0100

    Implement methods for animal children

--- task-8/src/Cat.java
+++ task-8/src/Cat.java
@@ -1,2 +1,21 @@
 public class Cat implements Animal2 {
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void travel() {
+
+    }
+
+    @Override
+    public void giveVoice() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
 }
--- task-8/src/Cow.java
+++ task-8/src/Cow.java
@@ -1,2 +1,21 @@
 public class Cow implements Animal2 {
+    @Override
+    public void eat() {
+        
+    }
+
+    @Override
+    public void travel() {
+
+    }
+
+    @Override
+    public void giveVoice() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
 }
