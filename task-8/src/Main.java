1ee68546bb9a98d47b8e5e6a75b3e3e5f2baf597
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 14:24:51 +0100

    move race method

--- task-8/src/Main.java
+++ task-8/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static void race(Moveable... moveable) {
+        for (Moveable m : moveable) {
+            m.start();
+            if (m instanceof Dog) {
+                System.out.printf("DOG: %s", ((Dog) m).getName());
+            }
+        }
+    }
+    
     public static void main(String[] args) {
-
+        System.out.println("RACE");
+        race(new Bike(), new Dog("Doggo"), new Car("Mcqueen"));
     }
 }
