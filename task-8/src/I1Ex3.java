30a027c83ebf33140f3bdef2e194027e6d2c86fc
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 14:44:37 +0100

    Create test

--- task-8/src/I1Ex3.java
+++ task-8/src/I1Ex3.java
@@ -1,5 +1,12 @@
 public class I1Ex3 {
     public static void main(String[] args) {
+        Plane plane = new Plane();
+        Ship ship = new Ship();
+        Hydroplane hydroplane = new Hydroplane();
 
+        plane.fly();
+        ship.swim();
+        hydroplane.fly();
+        hydroplane.swim();
     }
 }
