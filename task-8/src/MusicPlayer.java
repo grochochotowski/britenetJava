9c8e0fea4160f7ce8a0d4b44fb6840c4024ae58a
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 19:16:58 +0100

    Do media player class

--- task-8/src/MusicPlayer.java
+++ task-8/src/MusicPlayer.java
@@ -1,21 +1,27 @@
 public class MusicPlayer implements MediaPlayer {
+    private int VolumeLevel;
+
+    public MusicPlayer(int volumeLevel) {
+        VolumeLevel = volumeLevel;
+    }
+
     @Override
     public void play() {
-        
+        System.out.println("Music player start playing music");
     }
 
     @Override
     public void pause() {
-
+        System.out.println("Music paused");
     }
 
     @Override
     public void stop() {
-
+        System.out.println("Music player stopped playing music");
     }
 
     @Override
     public void adjustVolume(int level) {
-
+        this.VolumeLevel = level;
     }
 }
