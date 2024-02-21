19f73ddde2b8aa9c06060fa377e77fcf76791e03
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 19:08:21 +0100

    Create circle methods

--- task-8/src/Circle.java
+++ task-8/src/Circle.java
@@ -1,16 +1,19 @@
 public class Circle implements GeometricShape {
+
+    private double radius;
+
     @Override
     public void calculateArea() {
-
+        System.out.println("Area: " + (Math.PI*radius*radius));
     }
 
     @Override
     public void calculateCircumference() {
-
+        System.out.println("Circumference: " + (2*Math.PI*radius));
     }
 
     @Override
     public void draw() {
-
+        System.out.println("Drawing circle");
     }
 }
