8334544ae79795305a71c5a8b40a90a5b6e49097
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 19:18:21 +0100

    Do podcast player class

--- task-8/src/PodcastPlayer.java
+++ task-8/src/PodcastPlayer.java
@@ -1,21 +1,28 @@
 public class PodcastPlayer implements MediaPlayer {
+
+    private int VolumeLevel;
+
+    public PodcastPlayer(int volumeLevel) {
+        VolumeLevel = volumeLevel;
+    }
+
     @Override
     public void play() {
-
+        System.out.println("Podcast is going");
     }
 
     @Override
     public void pause() {
-
+        System.out.println("Podcast paused");
     }
 
     @Override
     public void stop() {
-
+        System.out.println("Podcast stopped");
     }
 
     @Override
     public void adjustVolume(int level) {
-
+        this.VolumeLevel = level;
     }
 }
