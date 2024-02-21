67c239f86979effe824a66d9562b4db64e025772
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 14:22:41 +0100

    add race method

--- task-8/src/Moveable.java
+++ task-8/src/Moveable.java
@@ -1,4 +1,13 @@
 public interface Moveable {
     void start();
     void stop();
+
+    public static void race(Moveable... moveable) {
+        for (Moveable m : moveable) {
+            m.start();
+            if (m instanceof Dog) {
+                System.out.printf("DOG: %s", ((Dog) m).getName());
+            }
+        }
+    }
 }
