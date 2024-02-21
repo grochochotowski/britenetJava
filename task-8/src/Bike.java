54174042010b9d8bd9a5fccb3ef0a973b3b3dcd9
grochochotowski <g.m.rochowski@gmail.com>
2024-02-21 13:59:52 +0100

    create bike class

---
+++ task-8/src/Bike.java
@@ -0,0 +1,10 @@
+public class Bike implements Moveable{
+    @Override
+    public void start() {
+        System.out.println("BIKE STARTED");
+    }
+    @Override
+    public void stop() {
+        System.out.println("BIKE STOPPED");
+    }
+}
