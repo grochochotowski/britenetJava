9228fccf4836808e80881b64a62d7e1592b3bff4
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:00:15 +0100

    Create patient class

---
+++ task-9/src/Patient.java
@@ -0,0 +1,8 @@
+public class Patient {
+    protected String name;
+    protected String surname;
+    protected String PESEL;
+    protected String wardName;
+    protected int daysStaying;
+    protected int age;
+}
