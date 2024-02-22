cec06743da554aa0a7ea69571df271650690e105
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:18:16 +0100

    add getters and setters

--- task-9/src/Hospital.java
+++ task-9/src/Hospital.java
@@ -4,11 +4,28 @@ public class Hospital {
     private int maxPatients;
     private ArrayList<Patient> patients;
 
+    // constructors
     public Hospital() {
     }
-
     public Hospital(int maxPatients, ArrayList<Patient> patients) {
         this.maxPatients = maxPatients;
+        this.patients = patients;
+    }
+
+    // getters setters
+    public int getMaxPatients() {
+        return maxPatients;
+    }
+
+    public void setMaxPatients(int maxPatients) {
+        this.maxPatients = maxPatients;
+    }
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(ArrayList<Patient> patients) {
         this.patients = patients;
     }
 }
