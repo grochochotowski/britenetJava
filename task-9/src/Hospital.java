446dd6a68c90ea5a3fca5e46f968b3a6e0fde02d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:17:45 +0100

    add constructors

--- task-9/src/Hospital.java
+++ task-9/src/Hospital.java
@@ -3,4 +3,12 @@ import java.util.ArrayList;
 public class Hospital {
     private int maxPatients;
     private ArrayList<Patient> patients;
+
+    public Hospital() {
+    }
+
+    public Hospital(int maxPatients, ArrayList<Patient> patients) {
+        this.maxPatients = maxPatients;
+        this.patients = patients;
+    }
 }
