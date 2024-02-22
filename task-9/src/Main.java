ff8306ad4ebdf812b2cda2884ca7c80bf71c5474
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:59:07 +0100

    refactor

--- task-9/src/Main.java
+++ task-9/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // patients
 
         ArrayList<Patient> patients = new ArrayList<>();
-        File file = new File("newpatients.txt");
+        File file = new File("C:\\nonSystem\\IT\\Code\\britenetJava\\task-9\\src\\newpatients.txt");
         Scanner scan = new Scanner(file);
 
         while (scan.hasNextLine()) {
@@ -20,6 +20,8 @@ public class Main {
         }
         scan.close();
 
+        System.out.println("\n\nInital:");
+        for (Patient patient : patients) patient.display();
         Random random = new Random();
         for (Patient patient : patients) {
             int newDays = random.nextInt(10) + 1;
@@ -40,12 +42,15 @@ public class Main {
                 return p1.getSurname().compareTo(p2.getSurname());
             }
         });
+        System.out.println("\n\nSOrt by name and surname:");
         for (Patient patient : patients) patient.display();
 
         patients.sort(Comparator.comparingInt(Patient::getDaysStaying));
+        System.out.println("\n\nSort by days staying:");
         for (Patient patient : patients) patient.display();
 
         patients.sort(Comparator.comparingInt(Patient::getAge));
+        System.out.println(" \n\nSort by age:");
         for (Patient patient : patients) patient.display();
     }
 }
