f223b756c11a3f4b74568faf3cb4f02d290296da
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:41:30 +0100

    display lsit

--- task-9/src/Main.java
+++ task-9/src/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         // patients
+
         ArrayList<Patient> patients = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         File file = new File("./newpatients.txt");
@@ -16,5 +18,9 @@ public class Main {
             Patient newPatient = new Patient(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], 0, Integer.parseInt(lineSplit[4]));
             patients.add(newPatient);
         }
+
+        for (Patient patient : patients) patient.display();
+
+
     }
 }
