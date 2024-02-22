8af2c1d7d81622e93ab987cec98e4522dc13b6e8
grochochotowski <g.m.rochowski@gmail.com>
2024-02-22 15:40:42 +0100

    Scan patients from txt to pogam

--- task-9/src/Main.java
+++ task-9/src/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // patients
+        ArrayList<Patient> patients = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        File file = new File("./newpatients.txt");
 
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] lineSplit = line.split(";");
+            Patient newPatient = new Patient(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], 0, Integer.parseInt(lineSplit[4]));
+            patients.add(newPatient);
+        }
     }
 }
