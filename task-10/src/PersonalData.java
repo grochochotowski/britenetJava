62a3f6b64c3afea73480fb57e4cfe657bd57ba0d
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 13:45:24 +0100

    add constructor to personal data class

--- task-10/src/PersonalData.java
+++ task-10/src/PersonalData.java
@@ -4,4 +4,12 @@ public class PersonalData {
     private final int year_of_birth;
     private String phone_number;
     private boolean payment;
+
+    public PersonalData(String name, String surname, int year_of_birth, String phone_number, boolean payment) {
+        this.name = name;
+        this.surname = surname;
+        this.year_of_birth = year_of_birth;
+        this.phone_number = phone_number;
+        this.payment = payment;
+    }
 }
