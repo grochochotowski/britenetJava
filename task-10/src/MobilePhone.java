dc3f5b770c2323e2b4fa6f437ee6a9946fd72272
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 14:00:57 +0100

    add constructors

--- task-10/src/MobilePhone.java
+++ task-10/src/MobilePhone.java
@@ -4,4 +4,14 @@ public class MobilePhone implements TelephoneOperator {
     private int talkTime;
     private int numberSMS;
     private PersonalData ownerData;
+
+    // constructors
+    public MobilePhone() {}
+    public MobilePhone(double priceMinute, double conversation, int talkTime, int numberSMS, PersonalData ownerData) {
+        this.priceMinute = priceMinute;
+        this.conversation = conversation;
+        this.talkTime = talkTime;
+        this.numberSMS = numberSMS;
+        this.ownerData = ownerData;
+    }
 }
