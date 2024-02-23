e5ccdcbb8c2c0b1d606bb58d9a77562d91a92e21
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 13:44:38 +0100

    Create personal data class

---
+++ task-10/src/PersonalData.java
@@ -0,0 +1,7 @@
+public class PersonalData {
+    private String name;
+    private String surname;
+    private final int year_of_birth;
+    private String phone_number;
+    private boolean payment;
+}
