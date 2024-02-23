f462893e030c786c75fd79221db6590a030fff69
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 13:43:01 +0100

    Create elephone operattor interface

---
+++ task-10/src/TelephoneOperator.java
@@ -0,0 +1,7 @@
+public interface TelephoneOperator {
+    public void call(int time);
+    public void bill();
+    public void newMonth();
+    public double totalPayment();
+    public void talkTimeInHours();
+}
