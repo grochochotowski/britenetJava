d81c0d0b123c7eb999ac40a788c539938c8ead6e
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 13:52:17 +0100

    Create base of mobile phone class

---
+++ task-10/src/MobilePhone.java
@@ -0,0 +1,7 @@
+public class MobilePhone implements TelephoneOperator {
+    private double priceMinute;
+    private double conversation;
+    private int talkTime;
+    private int numberSMS;
+    private PersonalData ownerData;
+}
