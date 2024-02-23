5b09ecfbe8b7d8b23000fb716d5a3746f9195e5b
grochochotowski <g.m.rochowski@gmail.com>
2024-02-23 14:36:23 +0100

    do point d

--- task-10/src/Main.java
+++ task-10/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         PersonalData data = new PersonalData("Name", "Surname", 2000, "999-888-777", true);
         MobilePhone phone = new MobilePhone(10, 5, data);
+
+        phone.call(30);
+        phone.sendSMS(3);
+        phone.sendSMS(3);
+        phone.bill();
+        phone.payBill();
     }
 }
